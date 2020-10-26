5309596c3df72b0d6f9ee18af367103187decb62
Shorya Singh Thakur <53837949+shorya97@users.noreply.github.com>
2020-10-26 13:50:27 +0530

    Create Search Insert Position.py

---
+++ DS & Algo/Week 2/Search Insert Position.py
@@ -0,0 +1,24 @@
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        n = len(nums)-1 
+        low = 0
+        high = n
+        while low<=high:
+            mid=(low+high)//2
+            
+            
+            if target==nums[mid]:
+                return mid
+            elif target>nums[mid]:
+                low=mid+1
+            else:
+                high=mid-1
+            
+        
+        if not nums:
+            return 0
+        if target>nums[high]:
+            return high+1
+        if target<nums[low]:
+            return low        
+        
