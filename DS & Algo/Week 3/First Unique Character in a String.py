61544aa7d4a2b80634d9f2f36d8b5673a19e3aff
Shorya Singh Thakur <53837949+shorya97@users.noreply.github.com>
2020-10-28 17:16:45 +0530

    Update First Unique Character in a String.py

--- DS & Algo/Week 3/First Unique Character in a String.py
+++ DS & Algo/Week 3/First Unique Character in a String.py
@@ -1,1 +1,12 @@
-
+class Solution:
+    def firstUniqChar(self, s: str) -> int:
+        dict={}
+        for i in s:
+            if i not in dict:
+                dict[i]=1
+            else:
+                dict[i]+=1
+        for i in range(len(s)):
+            if dict[s[i]] == 1:
+                return i
+        return -1
