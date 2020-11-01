14a303cd7699eeb3a2a6f585938a902995931b25
Shorya Singh Thakur <53837949+shorya97@users.noreply.github.com>
2020-11-01 21:41:20 +0530

    Update Insert Interval.py

--- DS & Algo/Week 2/Sorting/Insert Interval.py
+++ DS & Algo/Week 2/Sorting/Insert Interval.py
@@ -1,1 +1,21 @@
-
+class Solution:
+    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        i=0
+        answer=[]    #initialize empty array
+        while i<len(intervals) and newInterval[0]>intervals[i][0]: #append elements which are less than newInterval
+                answer.append(intervals[i])
+                i+=1
+                
+        if not answer or answer[-1][1] < newInterval[0]:  #last number in answer is smaller than newInterval[0], if true , append , else move forward 
+            answer.append(newInterval)
+        else:
+            answer[-1][1] = max(answer[-1][1],newInterval[1])
+            
+        while i<len(intervals):                           #add the remaining elements by constantly checking the over lapping intervals
+            if answer[-1][1] < intervals[i][0]:
+                answer.append(intervals[i])
+            else:
+                answer[-1][1] = max(answer[-1][1],intervals[i][1])
+            i+=1
+        return answer
+                
