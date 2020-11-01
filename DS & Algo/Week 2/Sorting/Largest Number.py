c4c8d8ac4b41451ecadad0e7ab7e0dccbfb43c0e
Shorya Singh Thakur <53837949+shorya97@users.noreply.github.com>
2020-11-01 17:44:25 +0530

    Update Largest Number.py

--- DS & Algo/Week 2/Sorting/Largest Number.py
+++ DS & Algo/Week 2/Sorting/Largest Number.py
@@ -1,1 +1,25 @@
-
+class Solution:
+    def largestNumber(self, nums: List[int]) -> str:
+        
+        if not any(map(bool, nums)):    # this is , if we encounter just zeroes, edge case 
+            return '0'
+        
+        nums = list(map(str, nums))     #  converting it into a string 
+        
+        if len(nums) <2:
+            return "".join(nums)        
+        
+        def compare(i,j):                                               # comparing the int values after adding and comparing the digits
+            return int(nums[i] + nums[j]) > int(nums[j] + nums[i])
+        
+        for i in range(len(nums)-1):
+            j=i+1
+            while i<len(nums) and j<len(nums):
+                if compare(i,j):
+                    pass
+                else:
+                    nums[j],nums[i] = nums[i],nums[j]
+                j+=1
+            
+        return "".join(nums)
+            
