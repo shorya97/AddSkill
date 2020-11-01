dbf1a45245fa4ddc6f699962feabfd4583b4abc1
Shorya Singh Thakur <53837949+shorya97@users.noreply.github.com>
2020-11-02 01:09:55 +0530

    Update Largest Perimeter Triangle.py

--- DS & Algo/Week 2/Sorting/Largest Perimeter Triangle.py
+++ DS & Algo/Week 2/Sorting/Largest Perimeter Triangle.py
@@ -1,1 +1,7 @@
-
+class Solution:
+    def largestPerimeter(self, A: List[int]) -> int:
+        A.sort(reverse=True)
+        for a,b,c in zip(A,A[1:],A[2:]):
+            if b+c>a:
+                return a+b+c
+        return 0
