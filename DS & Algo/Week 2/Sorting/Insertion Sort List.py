bb4aeec67947f0dd70bdba7667a296b8e63d981d
Shorya Singh Thakur <53837949+shorya97@users.noreply.github.com>
2020-11-03 21:05:21 +0530

    Update Insertion Sort List.py

--- DS & Algo/Week 2/Sorting/Insertion Sort List.py
+++ DS & Algo/Week 2/Sorting/Insertion Sort List.py
@@ -1,1 +1,33 @@
-
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution:    
+    def insertionSortList(self, head: ListNode) -> ListNode:
+        dummy = ListNode()
+        
+        curr =head
+        while curr:
+            prev_node = dummy
+            next_node = prev_node.next
+            while next_node:
+                if curr.val < next_node.val:
+                    break
+                prev_node = next_node
+                next_node = next_node.next
+            
+            next_item = curr.next
+            
+            curr.next = next_node
+            prev_node.next = curr
+            
+            curr = next_item
+            
+        return dummy.next
+            
+            
+                
+        
+        
+        
