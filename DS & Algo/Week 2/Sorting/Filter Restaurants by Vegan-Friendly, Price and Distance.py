2fb06953bab67d5522f305f5dc991f5b7ba59f79
Shorya Singh Thakur <53837949+shorya97@users.noreply.github.com>
2020-11-05 15:46:08 +0530

    Update Filter Restaurants by Vegan-Friendly, Price and Distance.py

--- DS & Algo/Week 2/Sorting/Filter Restaurants by Vegan-Friendly, Price and Distance.py
+++ DS & Algo/Week 2/Sorting/Filter Restaurants by Vegan-Friendly, Price and Distance.py
@@ -1,1 +1,8 @@
-
+class Solution:
+    def filterRestaurants(self, restaurants: List[List[int]], veganFriendly: int, maxPrice: int, maxDistance: int) -> List[int]:
+        if veganFriendly == 1:
+            restaurants = list(filter(lambda x: x[2] ==1 , restaurants))
+            
+        restaurants = list(filter(lambda x: x[3]<=maxPrice and x[4] <= maxDistance, restaurants))
+        restaurants.sort(key=lambda x: (x[1],x[0]), reverse = True)
+        return [i[0] for i in restaurants]
