7188df10f3fef60ce0aae64698d63182a9802c93
Shorya Singh Thakur <53837949+shorya97@users.noreply.github.com>
2020-11-05 22:22:48 +0530

    Update Next Greater Element II.py

--- DS & Algo/Week 2/Stack/Next Greater Element II.py
+++ DS & Algo/Week 2/Stack/Next Greater Element II.py
@@ -1,1 +1,13 @@
-
+class Solution:
+    def nextGreaterElements(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        answer = [-1]*n
+        stack = []
+        high = nums.index(max(nums)) if n>0 else 0
+        for i in range(high,high-n,-1):
+            while len(stack) !=0 and  nums[stack[-1]] <= nums[i]:
+                del stack[-1]
+            if len(stack) > 0:
+                answer[i] = nums[stack[-1]]
+            stack.append(i)
+        return answer
