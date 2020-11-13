c55237f2744d9f116f07412f4898fab2ce21cdbc
Shorya Singh Thakur <53837949+shorya97@users.noreply.github.com>
2020-11-13 16:44:06 +0530

    Update Kth Smallest Element in a BST.py

--- DS & Algo/Week 5/Kth Smallest Element in a BST.py
+++ DS & Algo/Week 5/Kth Smallest Element in a BST.py
@@ -1,1 +1,19 @@
-
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def kthSmallest(self, root: TreeNode, k: int) -> int:
+        answer = self.inorder(root)
+        return answer[k-1]
+    
+    
+    def inorder(self,root):
+        if root is None:
+            return []
+        return self.inorder(root.left) + [root.val] + self.inorder(root.right)
+    
+    
+            
